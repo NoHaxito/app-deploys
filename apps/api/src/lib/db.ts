14e1d42b52357fe3c9ae272bea288f8ff08477ae
= <=>
2024-02-10 01:28:57 +0000

    Updated 8 files

---
+++ apps/api/src/lib/db.ts
@@ -0,0 +1,59 @@
+// @ts-ignore
+import pkg from 'pg';
+const { Pool } = pkg;
+import { Kysely, PostgresDialect } from 'kysely';
+import { env } from './env';
+
+const config = {
+	user: env.POSTGRESQL_USER,
+	password: env.POSTGRESQL_PASSWORD,
+	host: env.POSTGRESQL_HOST,
+	port: 19091,
+	database: 'defaultdb',
+	ssl: {
+		rejectUnauthorized: true,
+		ca: env.POSTGRESQL_SSL_CERTIFICATE
+	}
+};
+
+export const pool = new Pool(config);
+
+export const db = new Kysely<Database>({
+	dialect: new PostgresDialect({
+		pool
+	})
+});
+
+interface Database {
+	apps: AppsTable;
+	auth_user: UserTable;
+	oauth_account: OauthAccountTable;
+	user_session: SessionTable;
+}
+
+interface AppsTable {
+	id: string;
+	name: string;
+	description: string;
+	domain: string;
+	user_id: string;
+}
+
+interface UserTable {
+	id: string;
+	username: string;
+	email: string;
+	avatar_url: string;
+	hashed_password: string;
+	role?: 'user' | 'admin';
+}
+interface SessionTable {
+	id: string;
+	user_id: string;
+	expires_at: Date;
+}
+interface OauthAccountTable {
+	provider_id: string;
+	provider_user_id: string;
+	user_id: string;
+}
---
+++ apps/db.sql
@@ -0,0 +1,22 @@
+CREATE TABLE apps (
+    id TEXT PRIMARY KEY,
+    name TEXT NOT NULL,
+    description TEXT NOT NULL,
+    domain TEXT NOT NULL,
+    user_id TEXT NOT NULL,
+    FOREIGN KEY(user_id) REFERENCES auth_user(id) ON DELETE CASCADE
+);
+
+CREATE TABLE apps_settings (
+    app_id TEXT PRIMARY KEY,
+    FOREIGN KEY(app_id) REFERENCES apps(id) ON DELETE CASCADE
+);
+
+CREATE TABLE apps_status (
+    app_id TEXT PRIMARY KEY,
+    status TEXT NOT NULL,
+    isStarting BOOLEAN NOT NULL,
+    isStopping BOOLEAN NOT NULL,
+    isRunning BOOLEAN NOT NULL,
+    FOREIGN KEY(app_id) REFERENCES apps(id) ON DELETE CASCADE
+);
